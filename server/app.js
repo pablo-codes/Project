329893811975890536deef33b7f16b5079744587
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-06-15 17:31:54 +0100

    env

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/myapp').then(() => {
   console.log("DB Connected Successfully")
 }).catch((err) => {
   console.log("Mongo not enabled locally")
-  mongoose.connect("mongodb+srv://okpani:pablo570@pablo.jdoepos.mongodb.net/test").then(() => {
+  mongoose.connect(process.env.MONGO).then(() => {
     console.log("DB Connected Successfully")
   }).catch((err) => {
     console.log("No internet connection")
